3aefbedeff6f5ca07e12460f256b4fd912bc2f17
Semih <46994029+sigma21@users.noreply.github.com>
2023-04-17 07:47:12 +0200

    Update resources.py

--- week_2/workspaces/resources.py
+++ week_2/workspaces/resources.py
@@ -99,13 +99,36 @@ def mock_s3_resource(context: InitResourceContext) -> MagicMock:
     return s3_mock
 
 
-@resource
-def s3_resource():
+@resource(
+    config_schema={
+        "bucket": Field(String),
+        "access_key": Field(String),
+        "secret_key": Field(String),
+        "endpoint_url": Field(String),
+    },
+    description="A resource that can run S3",
+)
+def s3_resource(context: InitResourceContext) -> S3:
     """This resource defines a S3 client"""
-    pass
+    return S3(
+        bucket=context.resource_config["bucket"], 
+        access_key=context.resource_config["access_key"], 
+        secret_key=context.resource_config["secret_key"], 
+        endpoint_url=context.resource_config["endpoint_url"]
+        )
 
 
-@resource
-def redis_resource():
+
+@resource(
+    config_schema={
+        "host": Field(String),
+        "port": Field(Int),
+    },
+    description="A resource that can run Redis",
+)
+def redis_resource(context:InitResourceContext):
     """This resource defines a Redis client"""
-    pass
+    return Redis(
+        host=context.resource_config["host"], 
+        port=context.resource_config["port"] 
+        )
